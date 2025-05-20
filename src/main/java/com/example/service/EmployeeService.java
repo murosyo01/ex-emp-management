d8317129e8e3dfa2dee467157683b1cfa27375af
murosyo01 <murozuka75@gmail.com>
2025-05-20 12:04:06 +0900

    empserviceにshowListを定義

--- src/main/java/com/example/service/EmployeeService.java
+++ src/main/java/com/example/service/EmployeeService.java
@@ -1,4 +1,21 @@
 package com.example.service;
 
+import com.example.domain.Employee;
+import com.example.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
 public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public List<Employee> showList(){
+        return employeeRepository.findAll();
+    }
 }
