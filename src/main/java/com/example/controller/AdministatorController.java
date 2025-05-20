cda3ab302b8b72ff97b24f4956b53283b7a4a173
murosyo01 <murozuka75@gmail.com>
2025-05-20 10:47:59 +0900

    insertメソッドの更新

--- src/main/java/com/example/controller/AdministatorController.java
+++ src/main/java/com/example/controller/AdministatorController.java
@@ -5,6 +5,7 @@ import com.example.form.InsertAdministratorForm;
 import com.example.form.LoginForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,9 +20,16 @@ public class AdministatorController {
     }
 
     @PostMapping("/insert")
-    public String insert(InsertAdministratorForm form){
+    public String insert(InsertAdministratorForm form, Model model){
         Administrator administrator = new Administrator();
-//        administrator.setName();
+        administrator.setName(form.getName());
+        administrator.setMailAddress(form.getMailAddress());
+        administrator.setPassword(form.getPassword());
+
+        model.addAttribute("mailAddress", form.getMailAddress());
+        model.addAttribute("password", form.getPassword());
+//        model.addAttribute("administrator", administrator);
+
         return "redirect:/administrator/";
     }
 
