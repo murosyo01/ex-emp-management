aea17838bea899535fa093090099c90436acae69
murosyo01 <murozuka75@gmail.com>
2025-05-20 10:55:18 +0900

    不要ファイルの削除およびクラス名の誤字修正

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -4,7 +4,6 @@ import com.example.domain.Administrator;
 import com.example.form.InsertAdministratorForm;
 import com.example.form.LoginForm;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/administrator")
-public class AdministatorController {
+public class AdministratorController {
 
     @GetMapping("/")
     public String toLogin(LoginForm form){
@@ -25,6 +24,8 @@ public class AdministatorController {
         administrator.setName(form.getName());
         administrator.setMailAddress(form.getMailAddress());
         administrator.setPassword(form.getPassword());
+
+
 
         model.addAttribute("mailAddress", form.getMailAddress());
         model.addAttribute("password", form.getPassword());
