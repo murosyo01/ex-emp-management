4b7f6b79c65934a895e69393a044579a66ca29b4
murosyo01 <murozuka75@gmail.com>
2025-05-20 12:09:19 +0900

    empControllerにshowListメソッドを定義

--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -1,4 +1,28 @@
 package com.example.controller;
 
+import com.example.domain.Employee;
+import com.example.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employee")
 public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/showList")
+    public String showList(Model model){
+        List<Employee> employeeList = employeeService.showList();
+
+        model.addAttribute("employeeList", employeeList);
+
+        return "employee/list";
+    }
 }
