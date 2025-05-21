cdefb107eaca12d5b20a5a1c58b88481eabdda0d
murosyo01 <murozuka75@gmail.com>
2025-05-21 14:12:15 +0900

    アドミンフォームにバリデーション追加

--- src/main/java/com/example/form/InsertAdministratorForm.java
+++ src/main/java/com/example/form/InsertAdministratorForm.java
@@ -1,14 +1,27 @@
 package com.example.form;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
+
 /**
  * 管理者情報を登録するフォーム
  */
 public class InsertAdministratorForm {
     /** 名前 */
+    @NotNull(message = "名前は必須です。")
+    @NotBlank(message = "名前を空文字にはできません。")
     private String name;
+
     /** メアド */
+    @NotBlank(message = "メールアドレスは必須です。")
+    @Email(message = "メールアドレスの形式が正しくありません。")
     private String mailAddress;
+
     /** パスワード */
+    @NotBlank(message = "パスワードは必須です。")
+    @Length(min = 3, max = 100, message = "パスワードは3文字以上、100文字以内である必要があります。")
     private String password;
 
     public String getName() {
